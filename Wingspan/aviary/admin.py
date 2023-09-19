5e95d6a0e9713dadb16e9fd7f3242d2c2f246934
Jonah <46250528+Jonah4chalk@users.noreply.github.com>
2023-09-19 14:21:08 -0700

    testing grabbing a static image, adjusted image dimensions

--- Wingspan/aviary/admin.py
+++ Wingspan/aviary/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import BirdCard, Habitat, Foods
+from .models import BirdCard, Habitat, Food, Board
 # Register your models here.
 
 admin.site.register(BirdCard)
-admin.site.register(Hab)
+admin.site.register(Board)
+admin.site.register(Habitat)
+admin.site.register(Food)
