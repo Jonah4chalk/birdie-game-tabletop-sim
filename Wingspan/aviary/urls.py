c0f36a80a10738ebec3caef3710315c790732691
Jonah <46250528+Jonah4chalk@users.noreply.github.com>
2023-09-26 18:17:57 -0400

    added new update view to the repo

--- Wingspan/aviary/urls.py
+++ Wingspan/aviary/urls.py
@@ -7,5 +7,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('board/<int:board_id>', views.board, name='board'),
-    path('<str:bird_name>/', views.detail, name='detail')
+    path('<str:bird_name>/', views.detail, name='detail'),
+    path(r'^update/(?P<pk>\d+)', views.BirdUpdateView.as_view(), name='birdcard_update')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
