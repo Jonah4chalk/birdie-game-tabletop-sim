572809978e8aa8f829edb9b9b00db5600bce84b8
Jonah <46250528+Jonah4chalk@users.noreply.github.com>
2023-11-17 21:36:38 -0800

    dir rename

--- birdie-game/birdie-game/asgi.py
+++ birdie-game/birdie-game/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for wingspan project.
+ASGI config for birdie game project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wingspan.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'birdie-game.settings')
 
 application = get_asgi_application()
